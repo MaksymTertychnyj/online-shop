3ae5a5a2172d6794ba67decc679f67efa4b42fbd
MaksymTertychnyj <maka12008@ukr.net>
2022-05-31 15:00:56 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from "./components/about/About";
+import Contacts from "./components/contacts/Contscts";
+import Home from "./components/home/Home";
+import NoMatch from "./components/no-match/NoMatch";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route element={<NoMatch />} />
+      </Routes>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
