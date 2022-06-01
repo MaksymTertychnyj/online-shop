a7e977025e067c99b66d0b12f85290d6c1177128
MaksymTertychnyj <maka12008@ukr.net>
2022-06-01 11:02:16 +0300

    changed App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import React, { Fragment } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import About from "./components/about/About";
-import Contacts from "./components/contacts/Contscts";
-import Home from "./components/home/Home";
-import NoMatch from "./components/no-match/NoMatch";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RoutesApp from "./navigation/routes/RoutesApp";
+import LoginProvider from "./providers/login-provider/LoginProvider";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contacts" element={<Contacts />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <LoginProvider>
+        <RoutesApp />
+      </LoginProvider>
+    </>
   );
 };
 
