c31b6d03d2310ae19fb478a2937a456a3ee15f15
MaksymTertychnyj <maka12008@ukr.net>
2022-06-01 19:54:03 +0300

    add user models, statusCodes

---
+++ src/models/user/UserAuthenticateModel.tsx
@@ -0,0 +1,11 @@
+type UserAuthenticateModel = null | {
+  firstName: string;
+  lastName: string;
+  email: string;
+  login: string;
+  password: string;
+  role: string;
+  token: string;
+};
+
+export default UserAuthenticateModel;
---
+++ src/models/user/UserModel.tsx
@@ -0,0 +1,10 @@
+type User = null | {
+  firstName: string;
+  lastName: string;
+  email: string;
+  login: string;
+  password: string;
+  role: string;
+};
+
+export default User;
