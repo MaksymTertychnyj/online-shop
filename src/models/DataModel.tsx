d3e9d5d6fd55662f357a58536c81e6baada56adc
MaksymTertychnyj <maka12008@ukr.net>
2022-06-06 08:12:46 +0300

    add models

---
+++ src/models/DataModel.tsx
@@ -0,0 +1,6 @@
+type DataModel = null | {
+  name: string;
+  value: any;
+};
+
+export default DataModel;
---
+++ src/models/image/ImageModel.tsx
@@ -0,0 +1,7 @@
+type ImageModel = null | {
+  imageData: Uint8Array;
+  targetId: number;
+  targetType: number;
+};
+
+export default ImageModel;
